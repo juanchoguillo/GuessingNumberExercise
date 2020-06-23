a3d2e6cfd2aaae3df8053b6b3480ce0e3f24f3ac
juanchoguillo <juancardona0607@gmail.com>
2020-06-23 10:25:04 -0400

    Guess numbers first files

---
+++ GuessingNumberExrecise/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GuessingNumberExrecise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 2);
+
+            Console.WriteLine("Try to Core");
+
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput == favNumber)
+            {
+                Console.WriteLine("Correct, Loocky Guess");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("IS to High!");
+            }
+
+            else
+            {
+                Console.WriteLine("Is to low!");
+            }
+
+        }
+            
+        }
+    }
+
